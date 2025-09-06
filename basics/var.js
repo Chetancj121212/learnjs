291fd64e5bcf3a1cb8e5ac2544f9e0dc75506bd9
Chetan Jharbade <211745710+Chetancj121212@users.noreply.github.com>
2025-09-06 08:25:16 +0000

    init

---
+++ basics/var.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accEmail = "cj@gmail.com"
+var accPass = "123456"
+accountCity = "Indore"
+
+accountId = 144553
+accEmail = "cj@gmail.com"
+accPass = "123456"
+accountCity = "Indore"
+
+console.log(accountId);
+console.log(accEmail);
+console.log(accPass);
+console.log(accountCity);
+console.table([accEmail,accPass,accountId,accountCity])
